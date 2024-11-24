f0128a9ec7df1b7e7b880a5d33282b0d65436e51
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-24 17:53:42 +0300

    refactor(lexer): add Literal token

--- crates/lexer/src/tokenizer/token.rs
+++ crates/lexer/src/tokenizer/token.rs
@@ -3,10 +3,17 @@ pub enum Token {
     Keyword(String),
     Identifier(String),
     Type(String),
-    Number(String),
+    Literal(Literal),
     Operator(String),
     Punctuation(char),
     Whitespace,
     Comment(String),
+    NewLine,
     EOF,
+}
+
+#[derive(Debug)]
+pub enum Literal {
+    String(String),
+    Number(String),
 }
