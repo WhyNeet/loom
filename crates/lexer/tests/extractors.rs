d0a0f2486fe16fbd5211b1a8fe96d531b35aaeb8
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-24 16:18:27 +0300

    test(lexer): add keyword extractor tests

--- crates/lexer/tests/extractors.rs
+++ crates/lexer/tests/extractors.rs
@@ -10,3 +10,20 @@ pub fn number_extractor_works() {
     let n2 = extractors::extract_number(&string[(n1.len() + 3)..]);
     assert_eq!(&n2, "33");
 }
+
+#[test]
+pub fn keyword_extractor_works() {
+    let string = "if a > b { return a; } else { return b; }";
+
+    let k1 = extractors::extract_keyword(string);
+    assert!(k1.is_some());
+    assert_eq!(k1.as_ref().unwrap(), "if");
+
+    let k2 = extractors::extract_keyword(&string[11..]);
+    assert!(k2.is_some());
+    assert_eq!(k2.as_ref().unwrap(), "return");
+
+    let k3 = extractors::extract_keyword(&string[23..]);
+    assert!(k3.is_some());
+    assert_eq!(k3.as_ref().unwrap(), "else");
+}
