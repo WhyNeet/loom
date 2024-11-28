88d0ef3e6a41ee92a98a31f0518b221563e12989
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-28 18:11:34 +0300

    feat(common): add keywords

---
+++ crates/common/src/constants/keywords.rs
@@ -0,0 +1,8 @@
+pub const DECLARATION_FUNCTION: &str = "fun";
+pub const DECLARATION_VARIABLE: &str = "let";
+pub const DECLARATION_CONSTANT: &str = "const";
+
+pub const STATEMENT_RETURN: &str = "return";
+pub const STATEMENT_IF: &str = "if";
+pub const STATEMENT_ELSE: &str = "else";
+pub const STATEMENT_WHILE: &str = "while";
