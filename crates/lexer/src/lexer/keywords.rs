94c6486dfd81e4d3cf1b07a5d2bd5cfeb28196a9
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-28 18:12:49 +0300

    refactor(lexer): replace literals with constants from common

--- crates/lexer/src/lexer/keywords.rs
+++ crates/lexer/src/lexer/keywords.rs
@@ -1,1 +1,14 @@
-pub const KEYWORDS: &[&str] = &["let", "fun", "if", "else", "while", "return", "const"];
+use common::constants::keywords::{
+    DECLARATION_CONSTANT, DECLARATION_FUNCTION, DECLARATION_VARIABLE, STATEMENT_ELSE, STATEMENT_IF,
+    STATEMENT_RETURN, STATEMENT_WHILE,
+};
+
+pub const KEYWORDS: &[&str] = &[
+    DECLARATION_CONSTANT,
+    DECLARATION_FUNCTION,
+    DECLARATION_VARIABLE,
+    STATEMENT_IF,
+    STATEMENT_ELSE,
+    STATEMENT_WHILE,
+    STATEMENT_RETURN,
+];
