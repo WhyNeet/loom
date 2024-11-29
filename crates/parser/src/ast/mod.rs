0d3d151b20b2908cf16d826820922b2959944a4a
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-11-29 15:04:01 +0300

    feat(parser): add operator precedence

--- crates/parser/src/ast/mod.rs
+++ crates/parser/src/ast/mod.rs
@@ -73,12 +73,68 @@ pub enum Literal {
     Char(char),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Operation {
+    Algebraic(AlgebraicOperation),
+    Logical(LogicalOperation),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum AlgebraicOperation {
     Addition,
     Subtraction,
     Multiplication,
     Division,
+}
+
+impl PartialOrd for AlgebraicOperation {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self == &AlgebraicOperation::Addition
+            || self == &AlgebraicOperation::Subtraction && other == &AlgebraicOperation::Division
+            || other == &AlgebraicOperation::Multiplication
+        {
+            Some(std::cmp::Ordering::Less)
+        } else if other == &AlgebraicOperation::Addition
+            || other == &AlgebraicOperation::Subtraction && self == &AlgebraicOperation::Division
+            || self == &AlgebraicOperation::Multiplication
+        {
+            Some(std::cmp::Ordering::Greater)
+        } else {
+            Some(std::cmp::Ordering::Equal)
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[repr(u8)]
+pub enum LogicalOperation {
+    Equal = 6,
+    GreaterOrEqual = 5,
+    LessOrEqual = 4,
+    Greater = 3,
+    Less = 2,
+    Or = 1,
+    And = 0,
+}
+
+impl PartialOrd for LogicalOperation {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let op1 = *self as u8;
+        let op2 = *other as u8;
+
+        op1.partial_cmp(&op2)
+    }
+}
+
+impl PartialOrd for Operation {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match (self, other) {
+            (Operation::Algebraic(_), Operation::Logical(_)) => Some(std::cmp::Ordering::Greater),
+            (Operation::Logical(_), Operation::Algebraic(_)) => Some(std::cmp::Ordering::Less),
+            (Operation::Algebraic(op1), Operation::Algebraic(op2)) => op1.partial_cmp(op2),
+            (Operation::Logical(log1), Operation::Logical(log2)) => log1.partial_cmp(log2),
+        }
+    }
 }
 
 #[derive(Debug)]
