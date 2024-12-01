b799c1754a4fc18432f4e3c9e5e07928efd428d1
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-12-01 19:46:15 +0300

    feat(lexer): add token as methods

--- crates/lexer/src/lexer/token.rs
+++ crates/lexer/src/lexer/token.rs
@@ -14,56 +14,56 @@ pub enum Token {
     EOF,
 }
 
-// impl Token {
-//     pub fn as_keyword(&self) -> Option<&str> {
-//         match self {
-//             Self::Keyword(value) => Some(value),
-//             _ => None,
-//         }
-//     }
+impl Token {
+    pub fn as_keyword(&self) -> Option<&str> {
+        match self {
+            Self::Keyword(value) => Some(value),
+            _ => None,
+        }
+    }
 
-//     pub fn as_identifier(&self) -> Option<&str> {
-//         match self {
-//             Self::Identifier(value) => Some(value),
-//             _ => None,
-//         }
-//     }
+    pub fn as_identifier(&self) -> Option<&str> {
+        match self {
+            Self::Identifier(value) => Some(value),
+            _ => None,
+        }
+    }
 
-//     pub fn as_type(&self) -> Option<&Type> {
-//         match self {
-//             Self::Type(value) => Some(value),
-//             _ => None,
-//         }
-//     }
+    pub fn as_type(&self) -> Option<&Type> {
+        match self {
+            Self::Type(value) => Some(value),
+            _ => None,
+        }
+    }
 
-//     pub fn as_literal(&self) -> Option<&Literal> {
-//         match self {
-//             Self::Literal(value) => Some(value),
-//             _ => None,
-//         }
-//     }
+    pub fn as_literal(&self) -> Option<&Literal> {
+        match self {
+            Self::Literal(value) => Some(value),
+            _ => None,
+        }
+    }
 
-//     pub fn as_operator(&self) -> Option<&str> {
-//         match self {
-//             Self::Operator(value) => Some(value),
-//             _ => None,
-//         }
-//     }
+    pub fn as_operator(&self) -> Option<&str> {
+        match self {
+            Self::Operator(value) => Some(value),
+            _ => None,
+        }
+    }
 
-//     pub fn as_punctuation(&self) -> Option<char> {
-//         match self {
-//             Self::Punctuation(value) => Some(*value),
-//             _ => None,
-//         }
-//     }
+    pub fn as_punctuation(&self) -> Option<char> {
+        match self {
+            Self::Punctuation(value) => Some(*value),
+            _ => None,
+        }
+    }
 
-//     pub fn as_comment(&self) -> Option<&str> {
-//         match self {
-//             Self::Comment(value) => Some(value),
-//             _ => None,
-//         }
-//     }
-// }
+    pub fn as_comment(&self) -> Option<&str> {
+        match self {
+            Self::Comment(value) => Some(value),
+            _ => None,
+        }
+    }
+}
 
 // impl PartialEq for Token {
 //     fn eq(&self, other: &Self) -> bool {
