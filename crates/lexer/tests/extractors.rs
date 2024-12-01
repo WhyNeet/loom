06b8dedde7c4b47db4f420a40cefa008ec780dd4
WhyNeet <117515380+WhyNeet@users.noreply.github.com>
2024-12-01 16:56:34 +0300

    test(lexer): add negative number extractor test

--- crates/lexer/tests/extractors.rs
+++ crates/lexer/tests/extractors.rs
@@ -3,13 +3,16 @@ use lexer::lexer::extractors;
 
 #[test]
 pub fn number_extractor_works() {
-    let string = "1234.56 + 33";
+    let string = "1234.56 + 33 + -5";
 
     let n1 = extractors::extract_number(string);
-    assert_eq!(&n1, "1234.56");
+    assert_eq!(n1, "1234.56");
 
     let n2 = extractors::extract_number(&string[(n1.len() + 3)..]);
-    assert_eq!(&n2, "33");
+    assert_eq!(n2, "33");
+
+    let n3 = extractors::extract_number(&string[(n1.len() + 3 + n2.len() + 3)..]);
+    assert_eq!(n3, "-5");
 }
 
 #[test]
