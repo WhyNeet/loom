714f80b3a387dd7f5fdaaf26bafd50183cee83ee
WhyNeet <arc.software.x@gmail.com>
2024-12-15 15:25:01 +0300

    feat(parser): add Parser struct

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,2 +1,18 @@
+use ast::AbstractSyntaxTree;
+use lexer::lexer::token::Token;
+use parser::parse;
+
 pub mod ast;
 pub mod parser;
+
+pub struct Parser {}
+
+impl Parser {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn run(&self, input: &[Token]) -> AbstractSyntaxTree {
+        AbstractSyntaxTree::new(parse(input).0)
+    }
+}
