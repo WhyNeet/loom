92bf5fa9a6384d4b72bacbb99b75f3bf2478a74b
WhyNeet <arc.software.x@gmail.com>
2024-12-22 18:16:49 +0300

    test(preprocessor): update control flow expression test

--- crates/preprocessor/tests/code_blocks.rs
+++ crates/preprocessor/tests/code_blocks.rs
@@ -1,12 +1,16 @@
 use std::rc::Rc;
 
 use lexer::lexer::Lexer;
-use parser::{ast::operation::AlgebraicOperation, Parser};
+use parser::{
+    ast::operation::{AlgebraicOperation, LogicalOperation},
+    Parser,
+};
 use preprocessor::{
     last::{
         declaration::{Declaration, VariableAllocation},
         expression::Expression,
         operation::Operation,
+        statement::Statement,
         unit::LASTUnit,
     },
     Preprocessor,
@@ -122,6 +126,95 @@ pub fn code_blocks_work() {
         LASTUnit::Declaration(Declaration::VariableDeclaration {
             allocation: VariableAllocation::Stack,
             identifier: "c".to_string(),
+            expression: Rc::new(Expression::Identifier("0".to_string()))
+        })
+    );
+}
+
+#[test]
+pub fn control_flow_as_expression_works() {
+    let code = r#"
+  let a = if b > c {
+    b
+  } else {
+    c
+  };
+"#;
+
+    let tokens = Lexer::new().run(code);
+    let ast = Parser::new().run(&tokens);
+
+    let last = Preprocessor::new().run(ast);
+
+    let root = last.root();
+
+    assert_eq!(
+        root[0],
+        LASTUnit::Declaration(Declaration::VariableDeclaration {
+            allocation: VariableAllocation::SSA,
+            identifier: "3".to_string(),
+            expression: Rc::new(Expression::Identifier("c".to_string()))
+        })
+    );
+
+    assert_eq!(
+        root[1],
+        LASTUnit::Declaration(Declaration::VariableDeclaration {
+            allocation: VariableAllocation::SSA,
+            identifier: "2".to_string(),
+            expression: Rc::new(Expression::Identifier("b".to_string()))
+        })
+    );
+
+    assert_eq!(
+        root[2],
+        LASTUnit::Declaration(Declaration::VariableDeclaration {
+            allocation: VariableAllocation::SSA,
+            identifier: "1".to_string(),
+            expression: Rc::new(Expression::BinaryExpression {
+                left: Rc::new(Expression::Identifier("2".to_string())),
+                right: Rc::new(Expression::Identifier("3".to_string())),
+                operation: Operation::Logical(LogicalOperation::Greater)
+            })
+        })
+    );
+
+    assert_eq!(
+        root[3],
+        LASTUnit::Statement(Statement::ControlFlow {
+            condition: Rc::new(Expression::Identifier("1".to_string())),
+            execute: vec![
+                LASTUnit::Declaration(Declaration::VariableDeclaration {
+                    allocation: VariableAllocation::SSA,
+                    identifier: "4".to_string(),
+                    expression: Rc::new(Expression::Identifier("b".to_string()))
+                }),
+                LASTUnit::Declaration(Declaration::VariableDeclaration {
+                    allocation: VariableAllocation::SSA,
+                    identifier: "0".to_string(),
+                    expression: Rc::new(Expression::Identifier("4".to_string()))
+                })
+            ],
+            alternative: Some(vec![
+                LASTUnit::Declaration(Declaration::VariableDeclaration {
+                    allocation: VariableAllocation::SSA,
+                    identifier: "5".to_string(),
+                    expression: Rc::new(Expression::Identifier("c".to_string()))
+                }),
+                LASTUnit::Declaration(Declaration::VariableDeclaration {
+                    allocation: VariableAllocation::SSA,
+                    identifier: "0".to_string(),
+                    expression: Rc::new(Expression::Identifier("5".to_string()))
+                })
+            ])
+        })
+    );
+
+    assert_eq!(
+        root[4],
+        LASTUnit::Declaration(Declaration::VariableDeclaration {
+            allocation: VariableAllocation::Stack,
+            identifier: "a".to_string(),
             expression: Rc::new(Expression::Identifier("0".to_string()))
         })
     );
